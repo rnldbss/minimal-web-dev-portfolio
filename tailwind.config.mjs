154d6b12d1201237bf0e49b607b86beed331a5e9
rnldbss <ronaldbessam@gmail.com>
2025-01-07 16:12:05 +0100

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+
+  theme: {
+    extend: {
+      colors: {
+      "background-light": '#f3f6f7',
+      "background-dark": '#213335',
+      "body-light": '#60767d',   
+      "body-dark": '#e7eef0',
+      accent: '#5BD1D7',
+      primary:"#b0bbbe"
+
+    }},
+
+    fontFamily: {
+      jakarta: ["Jakarta", "sans serif"],
+      michroma: ["Michroma", "sans serif"],
+      "pp-neue-montreal": ["PP Neue Montreal", "sans serif"]
+    },
+
+    
+  },
+
+  darkMode: ["class"],
+
+  plugins: [],
+};
