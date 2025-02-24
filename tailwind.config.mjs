0319ddd28ac8bf5f53beb0a60ca7b059cec79e04
rnldbss <ronaldbessam@gmail.com>
2025-02-24 21:27:18 +0000

    Tailwind config - adding new color system and doing the necessary changes on previous used classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,17 +5,37 @@ export default {
   theme: {
     extend: {
       colors: {
-      "background-light": '#f3f6f7',
-      "background-dark": '#213335',
-      "body-light": '#435156',   
-      "body-lighter": '#7b8589', 
-      "body-dark": '#e7eef0',
-      "heading-light": '#435358',   
-      "heading-dark": '#e7eef0',
-      accent: '#5BD1D7',
-      primary:"#b0bbbe"
-
-    }},
+        transparent: "transparent",
+        accent: {
+          primary: "#2bc2c2",
+          secondary: "#E43C13", //"#BAF400",
+        },
+        on: {
+          level: {
+            0: "#435358",
+            "0-dark": "#FAFCF8",
+          },
+          accent: {
+            primary: "#121212",
+            secondary: "#435358",
+          },
+        },
+        level: {
+          0: "#f3f6f7",
+          1: "#e6e8e980",
+          2: "#d8dbdb80",
+          3: "#cbcdce80",
+          4: "#bec0c080",
+          5: "#b1b3b380",
+          "0-dark": "#050e14", //Base background, used for large, non-interactive surfaces.
+          "1-dark": "#20252980", // Subtle elevation for small containers like buttons and cards.
+          "2-dark": "#373c4080", // Slightly more elevation for interactive elements.
+          "3-dark": "#50555880", // Used for modals, dialogs, and primary interactive elements.
+          "4-dark": "#6b6f7280", // Higher elevation for important interactive elements.
+          "5-dark": "#878a8c80", // Used for hover states, focus indicators, and highly interactive elements.
+        },
+      },
+    },
 
     fontFamily: {
       jakarta: ["Jakarta", "sans serif"],
@@ -23,10 +43,6 @@ export default {
       "pp-neue-montreal": ["PP Neue Montreal", "sans serif"],
       anek: ["Anek", "sans serif"],
     },
-
-  
-
-    
   },
 
   darkMode: ["class"],
