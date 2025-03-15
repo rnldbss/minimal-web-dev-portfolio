185d1c565fada72b12f818bdb888e2ffa0052cd8
rnldbss <ronaldbessam@gmail.com>
2025-03-15 00:17:41 +0000

    refactor: fixing contrast ratio

--- src/components/ButtonPrimary.jsx
+++ src/components/ButtonPrimary.jsx
@@ -1,8 +1,10 @@
-export default function ButtonPrimary({buttonHref, buttonText}) {
-  return  (<a
-  class="px-5 py-px rounded-sm inline-block bg-accent-primary text-on-level-0-dark w-max"
-  href={buttonHref}>{buttonText}</a>)
+export default function ButtonPrimary({ buttonHref, buttonText }) {
+  return (
+    <a
+      class="px-5 py-px rounded-sm inline-block ring hover:bg-accent-primary ring-accent-primary hover:text-on-level-0-dark transition-colors duration-75  w-max"
+      href={buttonHref}
+    >
+      {buttonText}
+    </a>
+  );
 }
- 
-   
-
